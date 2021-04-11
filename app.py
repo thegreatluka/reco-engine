72686f08e370e48a2a4ca256cc71f45cf8426e10
thegreatluka <sourav.sunil2004@gmail.com>
2021-04-12 04:13:26 +0530

    updated versions

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import flask
-import numpy as np
 import pandas as pd
 
 app = flask.Flask(__name__)
-reco_df_load = pd.read_pickle(r"pikles\user_final_rating_df.pkl")
-sent_df_load = pd.read_pickle(r"pikles\items_by_sentiment_score.pkl")
+reco_df_load = pd.read_pickle(r"https://github.com/thegreatluka/reco-engine/blob/master/pikles/user_final_rating_df.pkl?raw=true")
+sent_df_load = pd.read_pickle(r"https://github.com/thegreatluka/reco-engine/blob/master/pikles/items_by_sentiment_score.pkl?raw=true")
 
 @app.route('/')
 def home():
@@ -13,16 +12,14 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     if (flask.request.method == 'POST'):
-        int_features = [x for x in flask.request.form.values()]
-        final_features = [np.array(int_features)]
-        # output = model_load.predict(final_features).tolist()
-        output = reco_df_load.loc[int_features[0]].sort_values(ascending=False)[0:20]
+        features = [x for x in flask.request.form.values()]
+        output = reco_df_load.loc[features[0]].sort_values(ascending=False)[0:20]
         sorted_output = sent_df_load.loc[output.index.tolist()].sort_values(by='Pos%', ascending=False)[0:5]
-        res = flask.render_template('index.html', prediction_text='Product Recommendation for : {}'.format(int_features[0]),
+        res = flask.render_template('index.html', prediction_text='Product Recommendation for : {}'.format(features[0]),
                                     tables=[sorted_output.to_html(classes='data')], titles=sorted_output.columns.values)
         return res
     else:
-        return flask.render_template('index.html')
+        return flask.render_template('indexgi.html')
 
 @app.route("/predict_api", methods=['POST', 'GET'])
 def predict_api():
